efc56e0c0144889039e65e4a8f36104f81f719f7
Paul Ayora Yari <morfiayorayari@yahoo.com.ar>
2018-01-15 21:01:15 -0500

    Testing a commit activity

---
+++ appServiHogar/src/main/java/ec/edu/ups/app/util/Testing.java
@@ -0,0 +1,22 @@
+package ec.edu.ups.app.util;
+
+public class Testing {
+	
+	private int code;
+	private String description;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
