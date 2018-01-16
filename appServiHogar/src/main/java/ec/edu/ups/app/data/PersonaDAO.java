aeded91ab736ac6929047ac08002b36fed5bb1fa
Christian Novillo <christanov@gmail.com>
2018-01-16 17:38:54 -0500

    ingreso clase persona-categoria

--- appServiHogar/src/main/java/ec/edu/ups/app/data/PersonaDAO.java
+++ appServiHogar/src/main/java/ec/edu/ups/app/data/PersonaDAO.java
@@ -1,8 +1,11 @@
 package ec.edu.ups.app.data;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import ec.edu.ups.app.model.Persona;
 
@@ -22,12 +25,28 @@ public class PersonaDAO {
 		em.merge(persona);
 	}
 
-	public void borrar (String  cedula){
+	public void borrar (String cedula){
 		em.remove(leer(cedula));
 	}
+	
 	public Persona leer (String cedula){
-		em.find(Persona.class, cedula);
-		
-		return null;
-}
+		Persona p = em.find(Persona.class, cedula);
+		return p;
+	}
+	
+	public List<Persona> listadoPersonas(){
+		String jpql = "SELECT p FROM Persona p";
+		Query query = em.createQuery(jpql, Persona.class);
+		List<Persona> listado = query.getResultList();
+		return listado;
+	} 
+	
+	public void guardar(Persona persona){
+		Persona p = leer(persona.getCedula());
+		if (p==null)
+			insertar(persona);
+		else
+			actualizar(persona);
+	}
+
 }
--- appServiHogar/src/main/java/ec/edu/ups/app/model/CategoriaServicio.java
+++ appServiHogar/src/main/java/ec/edu/ups/app/model/CategoriaServicio.java
@@ -1,64 +1,6 @@
 
 package ec.edu.ups.app.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.NotBlank;
-
-@Entity
-@Table(name="tbl_cat_servicio")
-public class CategoriaServicio {
-	
-	@Id
-	@Column(name="cat_codigo")
-	private int codigo;
-	@NotBlank
-	@Size(min=4, max=60)
-	@Column(name="cat_nombre")
-	private String nombre;
-	
-	
-	@OneToMany(cascade = CascadeType.ALL,orphanRemoval = true)
-	@JoinColumn(name="catServicio",referencedColumnName="cat_codigo")
-	private List<Servicio> servicios;
-	
-	
-	
-	public List<Servicio> getServicios() {
-		return servicios;
-	}
-	public void setServicios(List<Servicio> servicios) {
-		this.servicios = servicios;
-	}
-	public int getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	
-
-}
-=======
-package ec.edu.ups.app.model;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
